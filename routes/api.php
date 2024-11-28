835a67b2b4aa7ed68209ff2f163b751c5021a5e8
manish <manishdhapse7@gmail.com>
2024-11-28 16:01:46 +0530

    added the api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserAuthControllers;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(){
+    return ['name' =>"Manish"];
+});
+
+Route::post('/signup',[UserAuthControllers::class,'signup']);
+Route::post('/login',[UserAuthControllers::class,'login']);
+
+Route::group(['middleware'=> 'auth:sanctum'],function () {
+    Route::get('/students',[StudentController::class,'list'] );
+    Route::post('/add-student',[StudentController::class,'addStudent']);
+    Route::put('/update-student',[StudentController::class,'updateStudent']);
+    Route::delete('/delete-student/{id}',[StudentController::class,'deleteStudent']);
+    Route::delete('/search-student/{name}',[StudentController::class,'searchStudent']);
+
+
+});
+
+
+
